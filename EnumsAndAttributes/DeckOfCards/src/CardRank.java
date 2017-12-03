317dc8b78eef638d43c70a56367cd0a0736408a7
Nikola Vodenicharov <nikolavodeni4arov@gmail.com>
2017-12-03 20:45:01 +0200

    Card rank.

---
+++ EnumsAndAttributes/DeckOfCards/src/CardRank.java
@@ -0,0 +1,26 @@
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+    @Override
+    public String toString(){
+        String formatted =
+                String.format(
+                        "Ordinal value: %d; Name value: %s",
+                        this.ordinal(),
+                        this.name());
+
+        return formatted;
+    }
+}
--- EnumsAndAttributes/DeckOfCards/src/Main.java
+++ EnumsAndAttributes/DeckOfCards/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Card Suits:");
-        for (CardSuit cardSuit : CardSuit.values()) {
-            System.out.println(cardSuit);
+        System.out.println("Card Ranks:");
+        for (CardRank card : CardRank.values()) {
+            System.out.println(card);
         }
     }
 }
