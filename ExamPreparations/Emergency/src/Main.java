9a704399b0c4d88c9343ff7b512bd7fab79ffaf8
Nocholas <nikolavodeni4arov@gmail.com>
2018-05-01 18:37:31 +0300

    add print hi

--- ExamPreparations/Emergency/src/Main.java
+++ ExamPreparations/Emergency/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        // made checking for the setters inputs - not null, not only digits, not less than 3-4 characters. For intigers - not negative.
+        // made checking for the setters inputs - not null, not only digits, not less than 3-4 characters. For integers - not negative.
+
+        System.out.println("hi");
     }
 }
