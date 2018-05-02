548c9be5e628a8292794e0a2057321c0ff6e5355
NikolaVodenicharov <nikolavodeni4arov@gmail.com>
2018-05-02 23:24:02 +0300

    Add in main.

--- Reflection/06. Java-OOP-Advanced-Reflection-Exercises-Skeleton/src/pr0304Barracks/Main.java
+++ Reflection/06. Java-OOP-Advanced-Reflection-Exercises-Skeleton/src/pr0304Barracks/Main.java
@@ -1,18 +1,27 @@
 package pr0304Barracks;
 
-import pr0304Barracks.contracts.Repository;
+import pr0304Barracks.contracts.*;
 import pr0304Barracks.contracts.Runnable;
-import pr0304Barracks.contracts.UnitFactory;
+import pr0304Barracks.core.CommandInterpreterImpl;
 import pr0304Barracks.core.Engine;
+import pr0304Barracks.core.factories.CommandFactoryImpl;
 import pr0304Barracks.core.factories.UnitFactoryImpl;
 import pr0304Barracks.data.UnitRepository;
 
+import java.lang.reflect.Constructor;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
 		Repository repository = new UnitRepository();
 		UnitFactory unitFactory = new UnitFactoryImpl();
-		Runnable engine = new Engine(repository, unitFactory);
+
+        CommandFactory commandFactory = new CommandFactoryImpl(repository, unitFactory);
+
+        CommandInterpreter commandInterpreter = new CommandInterpreterImpl(commandFactory);
+
+        Runnable engine = new Engine(commandInterpreter);
+
 		engine.run();
 	}
 }
