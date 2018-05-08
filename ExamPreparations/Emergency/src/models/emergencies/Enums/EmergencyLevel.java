8a7dfc2bd22f78fcd390dd11f11a2011651be200
NikolaVodenicharov <nikolavodeni4arov@gmail.com>
2018-05-08 11:58:41 +0300

    Enums add toString()

--- ExamPreparations/Emergency/src/models/emergencies/Enums/EmergencyLevel.java
+++ ExamPreparations/Emergency/src/models/emergencies/Enums/EmergencyLevel.java
@@ -3,5 +3,13 @@ package models.emergencies.Enums;
 public enum EmergencyLevel {
     MINOR,
     MAJOR,
-    DISASTER
+    DISASTER;
+
+    @Override
+    public String toString(){
+        String lowerCaseName = this.name().toLowerCase();
+        String titleCaseName = lowerCaseName.replace(this.name().substring(0,1), lowerCaseName.substring(0,1));
+
+        return titleCaseName;
+    }
 }
